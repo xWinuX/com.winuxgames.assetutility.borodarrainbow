8e1b1d4189a025a085cae28298b3e9d68b67df70
WinuX <37900754+xWinuX@users.noreply.github.com>
2022-04-09 20:01:15 +0200

    V1.0.0 Test

---
+++ Runtime/RainbowFolderSync.cs
@@ -0,0 +1,20 @@
+﻿using Borodar.RainbowHierarchy;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace WinuXGames.AssetUtility.BorodarRainbow
+{
+    [ExecuteAlways]
+    public class RainbowFolderSync : MonoBehaviour
+    {
+        #if UNITY_EDITOR
+        private void OnEnable() { HierarchyRuleset.OnRulesetChangeCallback += OnRulesetChange; }
+
+        private void OnDisable() { HierarchyRuleset.OnRulesetChangeCallback -= OnRulesetChange; }
+
+        private void OnRulesetChange() { PrefabUtility.ApplyPrefabInstance(gameObject, InteractionMode.UserAction); }
+        #endif
+    }
+}
